74e6bfa7565164243ac19a7b51a5053274aa6692
Sandesh-Vidhye <8767134178sandesh@gmail.com>
2025-05-29 00:14:35 +0530

    Data Types in Java

---
+++ basics_01/basics_01.java
@@ -0,0 +1,46 @@
+// ************* Basic Code ********************
+// public class basics_01 {
+
+//     public static void main (String args[]){
+//         System.out.println("Hello World  ");
+//         System.out.println("Hello World  ");
+//         System.out.println("ofijvinjvnsvn  ");
+
+//     }
+// }
+
+// ******************* Variables and Data Types ***********************
+// public class basics_01{
+    // public static void main(String args[]){
+//         int a = 10;
+//         int b = 6;
+//         System.out.println(a);
+//         System.out.println(b);
+
+//         String name = "SuperMan";
+//         System.out.println(name);
+
+//         a = 1000;
+//         System.out.println(a);
+
+//         // Datatypes 
+//         byte b = 8;
+//         char ch = "a";
+//         boolean IsLogin = true;
+//         float price = 10.5;
+//         int num = 25;
+        
+//     }
+// }
+
+// Data Types In Java 
+/*
+ 1.Primitive 
+ >byte-1bytes, short-2, char-2, boolean-1, int-4, long-8, float-4, double-8
+
+ 2.Non-Primitive 
+ > Sting, Array, Class, Object, Inteface
+
+ */
+
+// ******************* Sum of a and b ***********************
