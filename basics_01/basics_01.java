a31a8498c644163ca06be62e7dbde7ebf4559ff5
Sandesh-Vidhye <8767134178sandesh@gmail.com>
2025-05-30 00:25:06 +0530

    basic of Operators

--- basics_01/basics_01.java
+++ basics_01/basics_01.java
@@ -44,3 +44,97 @@
  */
 
 // ******************* Sum of a and b ***********************
+// public class basics_01 {
+//     public static void main(String[] args) {
+//         int a = 10 ;
+//         int b = 5 ;
+//         int sum = a+ b ;
+//         // System.out.println(sum);
+//         System.out.println(a+b);
+//     }
+// }
+
+// ************* Input In Java ********************
+
+// import java.util.Scanner;
+// public class basics_01 {
+//    public static void main(String[] args) {
+//     Scanner sc =  new Scanner(System.in);
+//     // String Input = sc.next();
+//     // System.out.println( " Name: " + Input);
+
+//     String Input = sc.nextLine();
+//     System.out.println( " Name: " + Input);
+     
+//     int num = sc.nextInt();
+//     System.out.println(num);
+
+//     float num1 = sc.nextFloat();
+//     System.out.println(num1);
+
+//    }
+// }
+
+
+// ************ Input from user *****************
+// import java.util.Scanner;
+// public class basics_01 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // int a = sc.nextInt();
+//         // int b = sc.nextInt();
+//         // int sum = a + b;
+//         // System.out.println(sum);
+
+//         // Area of Circle
+//         float radius = sc.nextFloat();
+//         System.out.println("Area: " + 3.14*radius*radius);
+
+//     }
+// }
+
+
+//*********************** Type Conversion ********************
+// public class basics_01 {
+
+//     public static void main(String[] args) {
+//         // char c = 'c';
+//         // int num = c;
+//         // System.out.println(num);
+
+//         char a = 'a';
+//         char b = 'b';
+//         char c ='c';
+//         System.out.println(b-a);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- basics_01/basics_02.java
+++ basics_01/basics_02.java
@@ -1,5 +1,12 @@
+// ********** OPERATORS *********************
 public class basics_02 {
   public static void main(String[] args) {
-    System.out.println("hello");
+    /* Arithmetic + - * / ++ -- %
+     Rational 
+     Logical 
+     Bitwise  
+     Assignment  */
+
+    
   }
 }
